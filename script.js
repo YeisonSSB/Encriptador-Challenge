6191504df3ddc1a6a4db297185e7b6a6981741b6
Yeison S <yeisonssb@gmail.com>
2023-05-26 16:18:21 -0500

    Justificando contenido de la pagina

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const textarea = document.querySelector("textarea");
+const mensaje = document.querySelector("mensaje");
+const copia = document.querySelector("copiar");
+copia.style.display = "none"
+
 textarea.addEventListener("keyup", (e) =>{
     let  scHeight = e.target.scrollHeight;
     textarea.style.height = "auto";
