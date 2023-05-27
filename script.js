44ce718e9f39d97c53844eb97e55653440125eec
Yeison S <yeisonssb@gmail.com>
2023-05-27 01:40:57 -0500

    Volviendo responsive la pagina

--- script.js
+++ script.js
@@ -4,11 +4,7 @@ const mensaje = document.querySelector("mensaje");
 const copia = document.querySelector("copiar");
 copia.style.display = "none"
 
-textarea.addEventListener("keyup", (e) =>{
-    let  scHeight = e.target.scrollHeight;
-    textarea.style.height = "auto";
-    textarea.style.height = `${scHeight}px`;
-});
+
 
 function validarTexto(){
     let textoEscrito = document.querySelector("textarea").value;
